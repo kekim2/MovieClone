f543b6cbbb801cdeb7bf2764c225a921e6ffbd62
KunEui <kuney930216@gmail.com>
2020-05-01 11:05:02 -0400

    MovieClone

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'e43124f13c787d2c74762f33b224b454';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
